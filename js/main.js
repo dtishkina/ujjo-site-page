256912835642c75317a492723924cff64df37ddf
dtishkina <lfhmztshkn@gmail.com>
2024-07-01 10:32:24 +0300

    add hero section

--- js/main.js
+++ js/main.js
@@ -2,4 +2,5 @@ import {loadSection} from "/js/loader.js";
 
 document.addEventListener('DOMContentLoaded', () => {
     loadSection('sections/header.html', 'header');
+    loadSection('sections/hero.html', 'hero');
 });
