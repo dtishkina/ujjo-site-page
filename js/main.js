397045c380e07974b09529900e30e8f7436c3720
dtishkina <lfhmztshkn@gmail.com>
2024-07-02 11:33:49 +0300

    fix file path

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-import {loadSection} from "/js/loader.js";
+import {loadSection} from "./js/loader.js";
 
 document.addEventListener('DOMContentLoaded', () => {
     loadSection('sections/header.html', 'header');
